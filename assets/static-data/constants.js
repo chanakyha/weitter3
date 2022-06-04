73067b4975123839c59aab5be4538c066ca21a70
Chanakyha <vetrichanakyha2003@gmail.com>
2022-06-04 23:56:32 +0530

    Smart Contract is Done

---
+++ assets/static-data/constants.js
@@ -0,0 +1,4 @@
+import contractArtifact from "./ProfileImageNfts.json";
+
+export const contactAddress = "0x418b946B36E018d1840c82E14f6b2654457D3C49";
+export const contractABI = contractArtifact.abi;
